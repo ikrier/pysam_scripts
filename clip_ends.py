ebb71177d27324d175a2f6fe3be1bc43c6f48cb8
Irina Krier <irina.krier@gmail.com>
2015-09-25 08:46:39 +0200

    fixed some things

--- clip_ends.py
+++ clip_ends.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python
+'''
+The code should do the following :
+	- read in a bam alignment file
+	- read in a list of amplicon regions
+	- clip the first 5 aligned bases of each read
+	- if the end of the read is at an amplicon end, clip the last 5 aligned bases
+Future improvements could include :
+	- keeping deletion structures by using a placeholder N before each deleted region
+	- verifying if the start of a read is at the start of an amplicon
+	- veriying if reads are paired
+	- allowing some uncertainty at the end of the amplicons since there's a 1/4 chance of aligning one more base, 
+	  and even more if the following bases are taken into account
+=======
+License
+=======
+This code is released under the GNU General Public License 3.0. A copy
+of this license is in the LICENSE.txt file.
+copyright Irina Krier 2015
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 import sys
 import pysam
@@ -6,14 +37,24 @@ import bbcflib
 from bbcflib.track import track
 import clip_functions
 from clip_functions import *
+import argparse
 
+parser = argparse.ArgumentParser(
+    description='Clip ends of reads according to amplicon structure')
+parser.add_argument('Amplicons bed file', metavar='amplicon_file.bed', type=str, nargs=1,
+                   help='A bed file containing the coordinates of the amplicons used in the design')
+parser.add_argument('Alignments bam', metavar='in_file.bam', type=str, nargs=1,
+                   help='A bam file containing the alignments to process')
+parser.add_argument('Output file', metavar='out_file.bam', type=str, nargs=1,
+                   help='A bam file containing the output')
 
+args=parser.parse_args()
+myargs=vars(args)
 
-samfile = pysam.AlignmentFile(sys.argv[2], "rb")
-pairedreads = pysam.AlignmentFile(sys.argv[3], "wb", template=samfile)
+samfile = pysam.AlignmentFile(myargs["Alignments bam"][0], "rb")
+pairedreads = pysam.AlignmentFile(myargs["Output file"][0], "wb", template=samfile)
 
-
-t = track(sys.argv[1],chrmeta="hg19")
+t = track(myargs["Amplicons bed file"][0],chrmeta="hg19")
 t.chrmeta["chrMT"]=t.chrmeta["chrM"]
 del t.chrmeta["chrM"]
 
